c9ae56f08ae32e306124f99b7af778ff5c75bcca
kunaldawane07 <154004918+kunaldawane07@users.noreply.github.com>
2024-04-17 12:09:11 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === in triple equal not converision but in double equal conversion would be performed
+
+console.log("2" === 2);
+
+
+// equality check and comparision are two different 
+
+// two different data type (null, undefined ) comparision should be avoid
